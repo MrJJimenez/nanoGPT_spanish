c8ba75cb9c146bcbd5a505f5114bba4779d6d546
Jesus Jimenez <jedjimenezsav1@gmail.com>
2024-03-10 18:36:52 +0100

    upload dataset

--- bigram.py
+++ bigram.py
@@ -1,3 +1,27 @@
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
 
-print('hola')
+# hyperparameters
+batch_size = 32 # how many independent sequences will we processin parallel
+block_size = 8 # what is the maximum content length for predictions?
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+
+torch.manual_seed(1337)
+
+# read it in to inspect it
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+print("length of dataset in characters: ", len(text))
+# let's look at the first 1000 characters
+#print(text[:1000])
+# here are all the unique characters that occur in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+print(''.join(chars))
+print(vocab_size)
