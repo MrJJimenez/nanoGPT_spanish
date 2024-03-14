5d07aa098b205822a0eca3800a034dcb75bec1df
Jesus Jimenez <jedjimenezsav1@gmail.com>
2024-03-14 20:51:05 +0100

    simple layer followed by ReLU

--- v2.py
+++ v2.py
@@ -102,7 +102,18 @@ class MultiHeadAttention(nn.Module):
     def forward(self, x):
         return torch.cat([h(x) for h in self.heads], dim=-1)
     
-        
+
+class FeedForward(nn.Module):
+    """ a simple layer followed by non-linearity"""
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd)
+            nn.ReLU(),
+        )
+    
+    def forward(self, x):
+        return self.net(x)
 
 # simply bigram model
 class BigramLanguageModel(nn.Module):
@@ -112,7 +123,7 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next tokon from a lookup
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(vocab_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.sa_heads = MultiHeadAttention(4, n_embd//4) # 4 heads of 8-dimensional self-attention
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
@@ -121,7 +132,7 @@ class BigramLanguageModel(nn.Module):
         tk_emb = self.token_embedding_table(idx) # Batch(4), Time(block_size), Channel(n_embd_size), (B, T, C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T, C)
         x = tk_emb + pos_emb # (B,T,C)
-        x = self.sa_head(x) # apply one head of self-attention. (B,T,C)
+        x = self.sa_heads(x) # apply one head of self-attention. (B,T,C)
         logits = self.lm_head(x)  # (Batch, Time, Vocab_size)
         
         if targets is None:
