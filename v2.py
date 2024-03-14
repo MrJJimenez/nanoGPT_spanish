1e7c748d01075eef38e110dd9e3acf53b996097e
Jesus Jimenez <jedjimenezsav1@gmail.com>
2024-03-14 20:37:19 +0100

    create multi head attention class

--- v2.py
+++ v2.py
@@ -93,6 +93,17 @@ class Head(nn.Module):
         out = wei @ v
         return out
 
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads =  nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+    
+        
+
 # simply bigram model
 class BigramLanguageModel(nn.Module):
     
